db53c9b3a5a50113c9527e4e969ec7d083ca3a09
Raphael Marcelino <110142433+raphaelfmarcelino@users.noreply.github.com>
2022-10-21 20:00:45 -0300

    Criacao de novos projetos e exercicios

---
+++ TesteTipos/src/testetipos/TesteTipos.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testetipos;
+
+/**
+ *
+ * @author raphaelmarcelino
+ */
+public class TesteTipos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int idade = 30;
+        //String valor = idade;
+        //String valor = (String) idade;
+        String valor = Integer.toString(idade);
+        System.out.println(valor);
+       
+    }
+    
+}
