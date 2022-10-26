0286005c6c8818b6df20968b5d535240888ec8a1
Raphael Marcelino <110142433+raphaelfmarcelino@users.noreply.github.com>
2022-10-26 13:52:29 -0300

    Operadores aritmeticos e classe math

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author raphaelmarcelino
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /*
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.println("A media e igual a " + m);
+        */
+        
+        /*
+        int numero = 5;
+        numero++;
+        System.out.println(numero);
+        */
+        
+        int x = 4;
+        x *= 2;
+        System.out.println(x);
+    }
+    
+}
